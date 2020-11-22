6b7e6a5b0ba90ac5bcb11b874771c8e1133769f3
Le Minh Quang <quanglm1998@gmail.com>
2020-11-22 23:17:26 +0700

    updated biconnected component

--- code/BridgesArticulations.cc
+++ code/BridgesArticulations.cc
@@ -1,34 +1,28 @@
-vector<int> adj[MAXN];
-int num[MAXN];
-int low[MAXN];
-int tms;
-int root, nchild;
-
-void dfs(int u, int p = -1) {
-    num[u] = low[u] = ++tms;
-    for (int i = 0; i < (int) adj[u].size(); i++) {
-        int v = adj[u][i];
+void dfs(int u, int p) {
+    num[u] = low[u] = ++cnt;
+    st.push_back(u);
+    for (auto v : adj[u]) {
         if (!num[v]) {
-            if (u == root) nchild++;
+            if (u == 1) nChild++; // root = 1
             dfs(v, u);
-            if (low[v] >= num[u]) {
-                //u is an articulation point
+            if (low[v] >= num[u]) { // u is articulation point, EXCEPT for 1
+                isArticulation[u] = 1;
+                numComp++;
+                while (!st.empty()) {
+                    int x = st.back();
+                    st.pop_back();
+                    ls[numComp].push_back(x);
+                    if (x == v) break;
+                }
+                ls[numComp].push_back(u);
             }
-            if (low[v] > num[u]) {
-                //u -> v is a bridge
+            if (low[v] > num[u]) { // u - v bridge
+                nBridge++;
             }
             low[u] = min(low[u], low[v]);
-        }
-        else if (v != p) {
+        } else if (v != p) {
             low[u] = min(low[u], num[v]);
         }
     }
 }
-
-int main() {
-    dfs(root = 0);
-    if (nchild > 1) {
-        //root is an articulation point
-    }
-    return 0;
-}
+if (nChild <= 1) isArticulation[1] = 0;
