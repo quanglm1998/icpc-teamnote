ad7ce72fbb623dfcdaaa6292efe861ddb20ce590
Le Minh Quang <quanglm1998@gmail.com>
2020-11-22 23:13:03 +0700

    added lyndon factorization

---
+++ code/LyndonFactorization.cc
@@ -0,0 +1,40 @@
+// https://cp-algorithms.com/string/lyndon_factorization.html
+vector<string> duval(string const& s) {
+    int n = s.size();
+    int i = 0;
+    vector<string> factorization;
+    while (i < n) {
+        int j = i + 1, k = i;
+        while (j < n && s[k] <= s[j]) {
+            if (s[k] < s[j])
+                k = i;
+            else
+                k++;
+            j++;
+        }
+        while (i <= k) {
+            factorization.push_back(s.substr(i, j - k));
+            i += j - k;
+        }
+    }
+    return factorization;
+}
+string min_cyclic_string(string s) {
+    s += s;
+    int n = s.size();
+    int i = 0, ans = 0;
+    while (i < n / 2) {
+        ans = i;
+        int j = i + 1, k = i;
+        while (j < n && s[k] <= s[j]) {
+            if (s[k] < s[j])
+                k = i;
+            else
+                k++;
+            j++;
+        }
+        while (i <= k)
+            i += j - k;
+    }
+    return s.substr(ans, n / 2);
+}
